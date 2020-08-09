adafe7a1efa689d478d4c9e2ca2638bbcf744292
wngus <wngus4356@naver.com>
2020-08-09 22:45:00 +0900

    주말공부!~ArrayEx09까지

---
+++ src/step2_11/arrayEx/ArrayEx02.java
@@ -0,0 +1,23 @@
+package step2_11.arrayEx;
+
+public class ArrayEx02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] arr = new int[3];
+		arr[0] = 10;
+		arr[1] = 20;
+		arr[2] = 30;
+		
+		for(int i = 0; i<3; i++) {
+			System.out.println(arr[i]);
+		}
+		
+		int[] temp = {10,20,30};
+		for(int i = 0; i < 3; i++) {
+			System.out.println(temp[i]);
+		}
+	}
+
+}
---
+++ src/step2_11/arrayEx/ArrayEx06_정답.java
@@ -0,0 +1,43 @@
+package step2_11.arrayEx;
+
+import java.util.Scanner;
+
+/*
+ * # 학생성적관리 프로그램[3단계] : 예외처리
+ */
+
+public class ArrayEx06_정답 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scan = new Scanner(System.in);
+		
+		int[] hakbuns = {1001, 1002, 1003, 1004, 1005};
+		int[] scores  = {  87,   11,   45,   98,   23};
+		
+		// 문제) 학번을 입력받아 성적 출력
+		// 		단, 없는학번 입력 시 예외처리
+		// 예)
+		// 학번 입력 : 1002		성적 : 11점
+		// 학번 입력 : 1000		해당학번은 존재하지 않습니다.
+		
+		System.out.println("학번 입력 : ");
+		int hakbun = scan.nextInt();
+		
+		int check = -1;
+		for(int i = 0; i<5; i++) {
+			if(hakbuns[i]==hakbun) {
+				check = i;
+			}
+		}
+		
+		if(check == -1) {
+			System.out.println("해당학번은 존재하지않습니다");
+		}else {
+			System.out.println("scores[check]" + "점");
+		}
+
+	}
+
+}
