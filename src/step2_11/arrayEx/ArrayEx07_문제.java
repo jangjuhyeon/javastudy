c09c8d81af8dc9b246e8aef5e665038d5be69afc
wngus <wngus4356@naver.com>
2020-08-11 14:56:22 +0900

    8.11 시작하기전

--- src/step2_11/arrayEx/ArrayEx07_문제.java
+++ src/step2_11/arrayEx/ArrayEx07_문제.java
@@ -12,20 +12,10 @@ public class ArrayEx07_문제 {
 		int[] hakbuns = {1001, 1002, 1003, 1004, 1005};
 		int[] scores  = {  87,   11,   45,   98,   23};
 		
-		// 문제) 1등학생의 학번과 성적 출력 
+		// 문제) 1등학생의 학번과 성적 출력
 		// 정답) 1004번(98점)
-		
-		int maxScore = 0;
-		int maxIdx = 0;
-		for(int i=0; i<5; i++) {
-			if(maxScore < scores[i]) {
-				maxScore = scores[i];
-				maxIdx = i;
-			}
-		}
-		System.out.println("1등학생 = " + hakbuns[maxIdx] + "번(" + 
-		maxScore + "점");
 
-	}
+
+	}	
 
 }
