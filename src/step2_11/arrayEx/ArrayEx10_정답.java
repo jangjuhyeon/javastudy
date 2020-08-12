1b642925c2dcd95113af4d334d5749dfdbd2f108
user <wngus4356@naver.com>
2020-08-12 17:30:58 +0900

    8.12(ArrayEx12_문제ing)

--- src/step2_11/arrayEx/ArrayEx10_정답.java
+++ src/step2_11/arrayEx/ArrayEx10_정답.java
@@ -1,5 +1,6 @@
 package step2_11.arrayEx;
 
+import java.util.Random;
 import java.util.Scanner;
 
 /*
@@ -7,11 +8,12 @@ import java.util.Scanner;
  * 1. 숫자 7이 연속으로 3번 등장하면, 당첨복권이다.
  */
 
-public class ArrayEx10_문제 {
+public class ArrayEx10_정답 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scan = new Scanner(System.in);
+		Random ran = new Random();
 		int[] lotto = {0, 0, 7, 7, 7, 0, 0, 0};
 		//int[] lotto = {7, 0, 7, 7, 0, 0, 0, 0};
 		//int[] lotto = {7, 0, 7, 7, 7, 0, 7, 0};
@@ -23,9 +25,17 @@ public class ArrayEx10_문제 {
 			int sel = scan.nextInt();
 			
 			if(sel == 1) {
+				for(int i =  0; i < 7-2; i++) {
+					if(lotto[i]==7 && lotto[i+1]==7 &&lotto[i+2]==7) {
+						System.out.println("당첨");
+						//i = 7-2;
+					}
+				}
 		
 			}						
 		}
+		
+		
 
 
 	}
--- src/step2_11/arrayEx/Ex02.java
+++ src/step2_11/arrayEx/Ex02.java
@@ -12,20 +12,38 @@ public class Ex02 {
 		// 학번과 점수가 한쌍이다 1001:20 , 1002:45  , 1003:54
 		
 		// 문제1) 학번을 입력하면 점수 출력 
+		/*
+		System.out.println("학번 입력 : ");
+		int hakbun = scan.nextInt();
 		
-		
-			for(int i = 0; i<7; i++) {
-				System.out.println("학번 입력 : ");
-				int hakbun = scan.nextInt();
-				if(arr[i]%2 == hakbun) {
+			for(int i = 0; i<6; i++) {
+				if(arr[i] == hakbun) {
 					System.out.println("나의 점수는 " + arr[i+1]);
 				}
-				}
-				
+			}
+			*/	
+			int score[] = {1001, 20,43 , 1002 , 54,2 , 1003,76,6};
 			
+			// 학번과 국어 수학 점수이다 
+			// 예) 1001 == (20 , 43)
+			//    1002 == (54 , 2)
+			//    1003 == (76 , 6)
+			// 문제3)  점수의 합을 입력하면 번호 출력 
+			// 예)  63 ==> 1001 
+			// 예)  56 ==> 1002
+			// 예)  82 ==> 1003
+			System.out.println("국어+수학 점수 입력 : ");
+			int totJumsu = scan.nextInt();
+			for(int i = 0; i<9; i++) {
+				if(totJumsu == arr[i+i+1]) {
+					System.out.println("나의 점수는" + arr[i-1]);
+				}
+			}
+
+
 		}
 		
-			//for문으로 찾아서 보내
+			
 			
 			
 		}
